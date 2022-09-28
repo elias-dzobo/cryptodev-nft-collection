2c37649cb954acdf3ec4fa3b29b43305c53d3d53
elias-dzobo <elias.dzobo@gmail.com>
2022-09-28 16:05:11 +0000

    tokenURI

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x5c264ae96CD51C5452f91b13194EbC0AA0109FEC";
+const WHITELIST_CONTRACT_ADDRESS = "0xfad62cbafF493320a6Fe76C8394365E9E4BB7113";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://cryptodev-nft-collection-gamma.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
